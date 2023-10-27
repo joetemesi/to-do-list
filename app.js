383121e586efc84f9e9a64d9e7439bdd84b941f3
Joseph M. Temesi <joemunyendo@gmail.com>
2023-10-27 09:28:53 +0300

    Add render method

--- app.js
+++ app.js
@@ -8,9 +8,51 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
+
 // Define a route
 app.get('/', (req, res) => {
-  res.send('Hello, Express.js!');
+
+  var today = new Date();
+  var currentDate = today.getDay();
+  var day = "";
+
+//   if (currentDate === 0 || currentDate === 6){
+//     day = "weekend";
+//   } else {
+//     day = "weekday";
+//   }
+
+  switch (currentDate) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;    
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day= "Saturday";
+        break; 
+    default:
+        console.log(currentDate);
+  }
+  console.log(currentDate); 
+
+  res.render("list", {
+    dayDate: day
+});
 });
 
 // Start the server
